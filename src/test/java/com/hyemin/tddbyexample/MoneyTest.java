1eaee83226ef7329b0ecec4993d6652b780d2d1a
박혜민/백엔드개발팀 <hyemin@ktown4u.com>
2024-06-06 09:03:32 +0900

    add assertion for triangulation

--- src/test/java/com/hyemin/tddbyexample/MoneyTest.java
+++ src/test/java/com/hyemin/tddbyexample/MoneyTest.java
@@ -17,6 +17,7 @@ public class MoneyTest {
     @Test
     public void testEquality() {
         assertThat(new Dollar(5)).isEqualTo(new Dollar(5));
+        assertThat(new Dollar(5)).isNotEqualTo(new Dollar(6));
     }
 
     private class Dollar {
